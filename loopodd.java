914b1115cf04df335dc54d45aff52443cf50c0a7
vina786 <vinay76b@gmail.com>
2024-12-05 12:37:16 +0530

    Add files via upload

---
+++ loopodd.java
@@ -0,0 +1,11 @@
+public class loopodd {
+    public static void main(String args[])
+    {
+        int i;
+        for(i=1;i<=10;i+=2)
+        {
+            System.out.println(i);
+            
+        }
+    }
+}
