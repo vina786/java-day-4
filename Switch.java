4ddc114ad57570c0c279a358fbc4d793a9d38c9b
vina786 <vinay76b@gmail.com>
2024-12-05 12:01:17 +0530

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Switch
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int flag=0;
+        if(a>=18)
+        flag=1;
+        switch(flag)
+        {
+            case 0:
+            System.out.println("not eligible");
+            break;
+             
+            case 1:
+            System.out.println("eligible");
+            break;
+        }
+        
+    }
+}
