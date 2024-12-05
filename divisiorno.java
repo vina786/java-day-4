bc5e1a640b449742c9c835a9d5b409c26ec8a741
vina786 <vinay76b@gmail.com>
2024-12-05 14:11:27 +0530

    Add files via upload

---
+++ divisiorno.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class divisiorno {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            for(int j=1;j<=n;j++)
+            {
+                if(n%j==0)
+                System.out.println(j+"  ");
+            }
+            System.out.println( );
+
+            }
+        }
+    }
+    
