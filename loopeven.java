493c22216981529ba4bad31c248594010139ffa5
vina786 <vinay76b@gmail.com>
2024-12-05 12:37:38 +0530

    Add files via upload

---
+++ loopeven.java
@@ -0,0 +1,10 @@
+public class loopeven {
+    public static void main(String args[])
+    {
+        int i;
+        for(i=0;i<=10;i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
