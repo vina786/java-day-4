220dc81a63ec2801e97a0f5e5687f73a47471782
vina786 <vinay76b@gmail.com>
2024-12-05 15:49:31 +0530

    Add files via upload

---
+++ checkingroot.java
@@ -0,0 +1,10 @@
+public class checkingroot {
+    public static void main(String args[])
+    {
+        int n=16;
+        for(int i=1;i*i<=n;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
