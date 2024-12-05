7e6c3c6bb8102bd4de65b0b657c4b6a7239c11d4
vina786 <vinay76b@gmail.com>
2024-12-05 12:02:01 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,12 @@
+public class multiple {
+    public static void main(String arg[])
+    {
+        int a=5;
+        if((a&7)==0)
+        {
+            System.out.println("multiple by 4");
+        }else{
+            System.out.println("not multiple by 4");
+        }
+    }
+}
